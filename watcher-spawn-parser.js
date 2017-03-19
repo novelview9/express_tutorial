8110cb67a67ca0655e1e4c9f1a4b8d9caf870531
novelview9 <novelview9@gmail.com>
2017-03-19 23:10:36 +0900

    Update target files

---
+++ watcher-spawn-parser.js
@@ -0,0 +1,21 @@
+const fs = require('fs'),
+  spawn = require('child_process').spawn,
+  filename = process.argv[2]
+
+if (!filename) {
+  throw Error('A file to watch must be specified!')
+}
+
+fs.watch(filename, () => {
+  let
+    ls = spawn('ls', ['-lh', filename])
+    output = ' '
+  ls.stdout.on('data', () => {
+    let parts = output.split(/ls+/)
+    console.dir([parts[0], parts[4], parts[8]])
+
+  })
+})
+
+
+console.log('Now watching' + filename + 'for changes...')
